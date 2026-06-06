0a6e9208d2c90badf0fe1c77442d6c54b9b03d1d
Matheus Tavares <107817846+SrTavaresS@users.noreply.github.com>
2026-06-06 05:03:55 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const hamburgerButtons = document.querySelectorAll('.btn-hamburger');
 const navbar = document.querySelector('nav');
+const overlay = document.querySelector('#overlay');
 
 function menuToggle() {
-    hamburgerButtons.forEach((button) => button.classList.toggle('active'));
+    hamburgerButtons.forEach(button =>
+        button.classList.toggle('active')
+    );
+
     navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
 }
 
-hamburgerButtons.forEach((button) =>{
+hamburgerButtons.forEach(button => {
     button.addEventListener('click', menuToggle);
 });
+
+overlay.addEventListener('click', menuToggle);
